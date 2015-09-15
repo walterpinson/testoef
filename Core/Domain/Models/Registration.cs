ede73693871b95804ed2033498bba23bc02b9f85
Walter Pinson <walter@walterpinson.com>
2015-09-15 15:59:01 -0400

    #1.  Add a behavior to the Registration type.

--- Core/Domain/Models/Registration.cs
+++ Core/Domain/Models/Registration.cs
@@ -5,8 +5,15 @@ namespace Core.Domain.Models
     public class Registration
     {
         public Guid Id { get; set; }
-        public DateTime RespondedOn { get; set; }
+        public DateTime RespondedOn { get; private set; }
         public Name Name { get; set; }
         public string Message { get; set; }
+        public bool IsRegistered { get; private set; }
+
+        public void Register()
+        {
+            RespondedOn = DateTime.Now;
+            IsRegistered = true;
+        }
     }
 }
